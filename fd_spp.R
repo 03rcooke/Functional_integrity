e30d62a8f56cdf412b60247829b2ac82a2f3d3b0
03rcooke <rob_cooke@hotmail.co.uk>
2016-02-15 16:47:15 +0000

    Creating sitexspecies matrix

--- fd_spp.R
+++ fd_spp.R
@@ -1,2 +1,28 @@
-#install.packages("fossil")
+# check working directory
+getwd
 
+# load necessary packages
+
+##################### fossil ######
+if(require("fossil")){
+  print("fossil is loaded correctly")
+} else {
+  print("trying to install fossil")
+  install.packages("fossil")
+  if(require("fossil")){
+    print("fossil installed and loaded")
+  } else {
+    stop("could not install fossil")
+  }
+}
+
+##### setting up sitexspecies matrix ####
+
+# read data into R:
+UK <- read.csv("ALL_Species_Ecoregions.csv")
+
+# check the data:
+head(UK)
+str(UK)
+
+create.matrix(UK,tax.name="binomial",locality="eco_code")
